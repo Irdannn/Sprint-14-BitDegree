e423c0f03725f4efaa6e5f8d9aee6ec484b78258
irdannn <idanirdan001@gmail.com>
2022-12-02 23:04:44 +0700

    Finish

--- Assignment-5-to-Project/webpack.config.js
+++ Assignment-5-to-Project/webpack.config.js
@@ -1,70 +1,43 @@
-// Webpack uses this to work with directories
+// Webpack uses this to work with directories.
 const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 // This is the main configuration object.
-// Here, you write different options and tell Webpack what to do
+// Here, you write different options and tell Webpack what to do.
 module.exports = {
-
-  // Path to your entry point. From this file Webpack will begin its work
+  // Path to your entry point. From this file Webpack will begin its work.
   entry: './src/index.js',
 
   // Path and filename of your result bundle.
-  // Webpack will bundle all JavaScript into this file
+  // Webpack will bundle all JavaScript into this file.
   output: {
     path: path.resolve(__dirname, 'dist'),
     publicPath: '',
     filename: 'bundle.js'
   },
 
-  devServer: {
-    static: {
-      directory: path.join(__dirname, 'public'),
-    },
-    compress: true,
-    port: 9000,
-  },
-
   module: {
     rules: [
-        {
-          test: /\.js$/,
-          loader: 'babel-loader', 
-          exclude: /node_modules/ 
-        },
-        {
-          test: /\.(sa|sc|c)ss$/,
-          use: ['style-loader', 'css-loader', 'sass-loader'],
-        },
-        // Now we apply rule for images
-        {
-          test: /\.(sa|sc|c)ss$/,
-          use: [
-            {
-              // It gets all of the transformed CSS and extracts it into a separate
-              // single bundled file.
-              loader: MiniCssExtractPlugin.loader
-            }, 
-            {
-              loader: "css-loader",
-            },
-          ]
-        },
+      {
+        test: /\.js$/,
+        loader: 'babel-loader', 
+        exclude: /node_modules/ 
+      },
+      // Now we apply a rule for images
+      {
+        test: /\.(png|jpe?g|gif|svg)$/,
+        use: [
+               {
+                 //By using file-loader for these files.
+                 loader: "file-loader",
+               }
+           ]
+       }
     ]       
-},
+  },
 
-plugins: [
-  new HtmlWebpackPlugin({
-    template: path.join(__dirname, "src", "index.html"),
-  }),
-  new MiniCssExtractPlugin({
-    filename: "bundle.css"
-  })
-],
   // Default mode for Webpack is production.
   // Depending on mode Webpack will apply different things
   // on the final bundle. For now, we don't need production's JavaScript 
-  // minifying and other things, so let's set mode to development
+  // minifying and other things, so let's set the mode to development.
   mode: 'development'
 };
